386656adf70203f3495a1d9b4517f08199b76537
Peter Witham <peter.witham@gmail.com>
2019-05-28 01:13:55 -0500

    Added screenshot and updated details

--- site-config.js
+++ site-config.js
@@ -1,30 +1,30 @@
 module.exports = {
   // App Info
-  appstore_link         : null,                                  // Enter App Store URL.
-  playstore_link        : "https://pw.d.pr/Jq6gSV",                                  // Enter Google Play Store URL.
-  google_analytics_ID   : "",                                        // Enter Google Analytics ID or ""
-  presskit_download_link: null,                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file).
-  video_or_screenshot   : "screenshot",                                               // "screenshot" or "video"
-  app_url               : "https://cashontapgame.com",                            // Domain of your website without path_prefix.
-  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
-  app_name              : "Cash On Tap Mobile Game",
-  app_price             : "Free",
-  app_description       : "The idle game where having the most is always better.",
-  app_keywords          : ["mobile", "android", "app", "ios","mobile game", "game"],
+  appstore_link: null, // Enter App Store URL.
+  playstore_link: "https://pw.d.pr/Jq6gSV", // Enter Google Play Store URL.
+  google_analytics_ID: "", // Enter Google Analytics ID or ""
+  presskit_download_link: null, // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file).
+  video_or_screenshot: "screenshot", // "screenshot" or "video"
+  app_url: "https://cashontapgame.com", // Domain of your website without path_prefix.
+  path_prefix: "/", // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name: "Cash On Tap Mobile Game",
+  app_price: "Free",
+  app_description: "The idle game where having the most is always better.",
+  app_keywords: ["mobile", "android", "app", "ios", "mobile game", "game"],
 
   // Personal Info
-  your_name              : "UI Buzz",
-  your_link              : "https://imedadel.me",
-  your_city              : null,
-  email_address          : "uibuzzcom@gmail.com",
-  facebook_username      : null,
-  instagram_username     : null,
-  twitter_username       : "CashOnTapGame",
-  github_username        : null,
-  youtube_username       : null,
+  your_name: "UI Buzz",
+  your_link: "https://uibuzz.com",
+  your_city: null,
+  email_address: "uibuzzcom@gmail.com",
+  facebook_username: null,
+  instagram_username: null,
+  twitter_username: "CashOnTapGame",
+  github_username: uibuzz,
+  youtube_username: null,
 
   // Features List
-  features               : [
+  features: [
     {
       title: "The Goal",
       description:
@@ -62,23 +62,23 @@ module.exports = {
       fontawesome_icon_name: "magic",
     },
   ],
-  header_background             : "rgba(0, 80, 0, 1.0)",
-  topbar_title_color            : "#ffffff",
-  cover_overlay_color_rgba      : "rgba(20, 59, 20, 0.8)",
-  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
-  body_background_color         : "rgba(0, 40, 0, 1.0)",
-  primary_text_color            : "#000",
-  content_width                 : "1170px",
-  font                          : `"Helvetica Neue", sans-serif`,
-  link_color                    : "rgba(0, 255, 0, 1.0)",
-  app_title_color               : "#ffffff",
-  app_price_color               : "#ffffff",
-  app_description_color         : "#ffffff",
-  feature_title_color           : "rgba(0, 200, 0, 1.0)",
-  feature_text_color            : "#eeeeee",
+  header_background: "rgba(0, 80, 0, 1.0)",
+  topbar_title_color: "#ffffff",
+  cover_overlay_color_rgba: "rgba(20, 59, 20, 0.8)",
+  device_color: "black", // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color: "rgba(0, 40, 0, 1.0)",
+  primary_text_color: "#000",
+  content_width: "1170px",
+  font: `"Helvetica Neue", sans-serif`,
+  link_color: "rgba(0, 255, 0, 1.0)",
+  app_title_color: "#ffffff",
+  app_price_color: "#ffffff",
+  app_description_color: "#ffffff",
+  feature_title_color: "rgba(0, 200, 0, 1.0)",
+  feature_text_color: "#eeeeee",
   feature_icons_foreground_color: "#eeeeee",
   feature_icons_background_color: "rgba(0, 100, 0, 0.6)",
-  social_icons_foreground_color : "#ffffff",
-  social_icons_background_color : "rgba(0, 100, 0, 0.6)",
-  footer_text_color             : "#cccccc",
+  social_icons_foreground_color: "#ffffff",
+  social_icons_background_color: "rgba(0, 100, 0, 0.6)",
+  footer_text_color: "#cccccc",
 }
